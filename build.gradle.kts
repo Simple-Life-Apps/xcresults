a44c81c83527839c36ca597cb0bdc05209d382b5
Oleg Montak <oleg.montak@gmail.com>
2022-11-02 19:08:58 +0200

    Update build tools to build with java11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("com.palantir.graal") version("0.9.0")
+    id("com.palantir.graal") version("0.12.0")
 }
 
 group = "org.example"
@@ -9,18 +9,19 @@ version = "1.0-SNAPSHOT"
 description = "Allure Server Java Client"
 
 tasks.withType(Wrapper::class) {
-    gradleVersion = "7.4"
+    gradleVersion = "7.5"
 }
 
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
 graal {
     graalVersion(project.property("graalVersion") as String?)
+    javaVersion("11")
 
     mainClass("io.eroshenkoam.xcresults.XCResults")
     outputName("xcresults")
@@ -33,7 +34,7 @@ repositories {
 dependencies {
     annotationProcessor("info.picocli:picocli-codegen:4.1.4")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
     implementation("io.qameta.allure:allure-model:2.13.1")
     implementation("info.picocli:picocli:4.1.4")
     implementation("commons-io:commons-io:2.6")
